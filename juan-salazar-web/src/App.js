3c759f03c1dd67decc6a76e47cdb207b260d1d7d
Juan Salazar <jjsc2003@live.com>
2023-02-21 19:14:04 -0600

    Added basic files

--- juan-salazar-web/src/App.js
+++ juan-salazar-web/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+// Components
+
+import Landing from "./views/Landing";
+import Portfolio from "./views/Portfolio";
+import Contact from "./views/Contact";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="about" element={<Portfolio />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
   );
 }
-
-export default App;
---
+++ juan-salazar-web/src/views/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Components
+
+import Navbar from "../components/Navbar";
+
+export default function Portfolio() {
+    return(
+        <Navbar/>
+    )
+}
